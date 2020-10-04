57b127b5fd73e786b937c0c6bb809380acb904ce
Gornius <ninertominer@gmail.com>
2020-10-04 19:42:34 +0200

    added picom widget

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
+    {"Picom: ", "pgrep picom > /dev/null && echo ON || echo OFF", 10, 11},
     {"Vol: ", "echo $(pamixer --get-volume)%", 10, 10},
     {"Bat: ", "echo $(cat /sys/class/power_supply/BAT0/capacity)% $(if [ \"$(cat /sys/class/power_supply/BAT0/status)\" != \"Discharging\" ]; then echo \"⚡\"; fi;)" , 10, 0},
 	{"", "date '+%b %d %H:%M'",					5,		0},
